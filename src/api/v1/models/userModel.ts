818e086f14396a9bd997561622ce8c48ac3ed9ba
jasleen <jasleenkaurkalsi03@gmail.com>
2025-03-06 23:44:14 -0600

    Added Mongoose schema for users with UID, email, and role.

---
+++ src/api/v1/models/userModel.ts
@@ -0,0 +1,33 @@
+import { Schema, model } from "mongoose";
+
+// Define User Schema
+const UserSchema = new Schema(
+  {
+    uid: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "officer", "admin"],
+      default: "user",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+// Create and export User model
+const User = model("User", UserSchema);
+export default User;
